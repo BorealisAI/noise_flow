e78b381781d025e3216c4eabb8431094cb01a5d7
ABDOT440S\Abdo <abdoukamel@gmail.com>
2020-01-12 20:47:03 -0500

    Rendering sampled noisy images into sRGB.

--- sample_noise_flow.py
+++ sample_noise_flow.py
@@ -1,21 +1,27 @@
+import glob
 import logging
 import os
-import zipfile
-from os.path import exists
+
+import cv2
+from scipy.io import savemat
 
 from borealisflows.NoiseFlowWrapper import NoiseFlowWrapper
 from mylogger import add_logging_level
 import sidd.data_loader as loader
 import pandas as pd
 import numpy as np
-import random
 
 from sidd.data_loader import check_download_sidd
-
+from sidd.pipeline import process_sidd_image
+from sidd.raw_utils import read_metadata
+from sidd.sidd_utils import unpack_raw
 
 data_dir = 'data'
 sidd_path = os.path.join(data_dir, 'SIDD_Medium_Raw/Data')
 nf_model_path = 'models/NoiseFlow'
+
+samples_dir = os.path.join(data_dir, 'samples')
+os.makedirs(samples_dir, exist_ok=True)
 
 
 def main():
@@ -31,40 +37,58 @@ def main():
     # Prepare NoiseFlow
     noise_flow = NoiseFlowWrapper(nf_model_path)
 
-    # load data  (load your own images here)
-    clean_images, cam_iso_info = loader.load_data_threads(sidd_path, max_images=1)
-
-    # camera IDs and ISO levels related to the SIDD dataset
-    cam_iso_nlf = load_cam_iso_nlf()
-    n_cam_iso = cam_iso_nlf['cam_iso'].count()
-    iso_vals = [100.0, 400.0, 800.0, 1600.0, 3200.0]
-    cam_ids = [0, 1, 3, 3, 4]  # IP, GP, S6, N6, G4
-    cam_vals = ['IP', 'GP', 'S6', 'N6', 'G4']
-
     # sample noise and add it to clean images
-    batch_size = 128  # using batches is faster
-    indices = list(range(clean_images.shape[0]))
-    for i in range(0, len(indices), batch_size):
+    patch_size = 32
+    batch_size = 1  # using batches is faster
+    for sc_id in [10, 52, 64]:  # scene IDs
 
-        # clean images
-        batch_clean = clean_images[indices[i:i + batch_size]]
-        batch_info = cam_iso_info[i:i + batch_size]
+        # load images
+        noisy = loader.load_raw_image_packed(glob.glob(os.path.join(sidd_path, '%04d_*' % sc_id, '*NOISY_RAW_010.MAT'))[0])
+        clean = loader.load_raw_image_packed(glob.glob(os.path.join(sidd_path, '%04d_*' % sc_id, '*GT_RAW_010.MAT'))[0])
+        metadata, bayer_2by2, wb, cst2, iso, cam = read_metadata(
+            glob.glob(os.path.join(sidd_path, '%04d_*' % sc_id, '*METADATA_RAW_010.MAT'))[0])
 
-        # ISO and camera ID
-        cam_iso_idx = random.randint(0, n_cam_iso - 1)
-        row = cam_iso_nlf.iloc[cam_iso_idx]
-        cam = cam_vals.index(row['cam_iso'][:2])
-        iso = float(row['cam_iso'][3:])
+        if iso not in [100, 400, 800, 1600, 3200]:
+            continue
 
-        # sample noise
-        batch_noise = noise_flow.sample_noise_nf(batch_clean, 0.0, 0.0, iso, cam)
+        n_pat = 10
+        for p in range(n_pat):
 
-        # add to clean images (do not forget to clip)
-        batch_noisy = batch_clean + batch_noise
-        batch_noisy = np.clip(batch_noisy, 0.0, 1.0)
+            # crop patches
+            v = np.random.randint(0, clean.shape[1] - patch_size)
+            u = np.random.randint(0, clean.shape[2] - patch_size)
 
-        # TODO: save or feed to another module
-        # ...
+            clean_patch = clean[0, v:v + patch_size, u:u + patch_size, :]
+            noisy_patch = noisy[0, v:v + patch_size, u:u + patch_size, :]
+
+            clean_patch = np.expand_dims(clean_patch, 0)
+
+            # sample noise
+            noise_patch_syn = noise_flow.sample_noise_nf(clean_patch, 0.0, 0.0, iso, cam)
+
+            noise_patch_syn = np.squeeze(noise_patch_syn)[1:-1, 1:-1, :]
+            clean_patch = np.squeeze(clean_patch)[1:-1, 1:-1, :]
+            noisy_patch_syn = unpack_raw(np.clip(clean_patch + noise_patch_syn, 0.0, 1.0))
+            clean_patch = unpack_raw(clean_patch)
+            noisy_patch = unpack_raw(np.squeeze(noisy_patch)[1:-1, 1:-1, :])
+
+            # process
+            clean_patch_srgb = process_sidd_image(clean_patch, bayer_2by2, wb, cst2)
+            noisy_patch_srgb = process_sidd_image(noisy_patch, bayer_2by2, wb, cst2)
+            noisy_patch_syn_srgb = process_sidd_image(noisy_patch_syn, bayer_2by2, wb, cst2)
+            conc_im = np.concatenate([clean_patch_srgb, noisy_patch_srgb, noisy_patch_syn_srgb], axis=1)
+            conc_height, conc_width, _ = conc_im.shape
+
+            # save as .png
+            scale = 16
+            cv2.resize(conc_im, (conc_width * scale, conc_height * scale), interpolation=cv2.INTER_NEAREST)
+            save_fn = os.path.join(samples_dir, '%02d_%02d_%04d.png' % (sc_id, p, iso))
+            cv2.imwrite(save_fn, conc_im)
+
+            # save as .mat
+            save_mat_fn = os.path.join(samples_dir, '%02d_%02d_%04d.mat' % (sc_id, p, iso))
+            savemat(save_mat_fn, {'clean': clean_patch, 'noisy': noisy_patch, 'noisy_syn': noisy_patch_syn,
+                                  'metadata': metadata})
 
 
 def load_cam_iso_nlf():
